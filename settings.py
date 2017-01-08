4238f4727a6d332a198d280463344b1e8bc6fdd6
Oleg Kutsenko <padshyangel@mail.ru>
2017-01-08 21:09:34 +0200

    set username for db from readme

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql+pg8000://postgres:postgres@localhost/meetup_develop')
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql+pg8000://meetup_user:qwerty@localhost/meetup_develop')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 RESTPLUS_VALIDATE = True
